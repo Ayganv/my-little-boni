b354ce2025345e1ccefcc06351f07b317e147dfe
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2021-02-23 14:42:46 +0100

    Adds some folders and the core gameplay scene

---
+++ Assets/Scripts/WalkerScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkerScript : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime;
+    }
+}
