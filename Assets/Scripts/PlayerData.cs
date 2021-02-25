534d878fb3eddd3d349b181b60d0b74a385a9935
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2021-02-25 12:14:20 +0100

    adds PlayerData

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerData
+{
+    public static int PlayerExperience
+    {
+        get => PlayerPrefs.GetInt("PlayerExperience", 0);
+        set
+        {
+            if (value > ExperienceRequired())
+            {
+                value -= ExperienceRequired();
+                PlayerLevel++;
+            }
+            PlayerPrefs.SetInt("PlayerExperience", value);
+        }
+    }
+    
+    public static int PlayerLevel { get => PlayerPrefs.GetInt("PlayerLevel", 1); set => PlayerPrefs.SetInt("PlayerLevel", value); }
+    
+    const int baseExperienceRequired = 5;
+    const int experienceIncreasePerLevel = 1;
+    
+    public static int ExperienceRequired()
+    {
+        return ((PlayerLevel - 1) * experienceIncreasePerLevel) + baseExperienceRequired;
+    }
+
+    public static int ExperienceUntilLevelUp()
+    {
+        return ExperienceRequired() - PlayerExperience;
+    }
+}
