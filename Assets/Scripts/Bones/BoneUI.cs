deed1b0b51fcf84af88aa76bc5009ec15bd9cd73
Linus Vallinder <divinator.dev@gmail.com>
2021-03-02 14:27:42 +0100

    Can Now add bones and save bone collection. Bones are seen in the inventory screen

---
+++ Assets/Scripts/Bones/BoneUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneUI : MonoBehaviour
+{
+    public GameObject boneCard;
+
+    private void Start()
+    {
+        SaveSystem.LoadGame();
+        CreateUIElements();
+    }
+
+    public void CreateUIElements()
+    {
+        foreach (var bone in BoneCollection.boneData.bones)
+        {
+            var clone = Instantiate(boneCard, this.transform);
+            clone.GetComponent<BoneCard>().bone = bone;
+        }
+    }
+}
